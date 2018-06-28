a780d54b44a1acb527d0afb8702fc613e0a6c8ec
Wate <31439027+Watesoyan@users.noreply.github.com>
2018-06-28 17:04:18 +0800

    Create #845. Longest Mountain in Array.py

---
+++ Solutions/#845. Longest Mountain in Array.py
@@ -0,0 +1,42 @@
+class Solution:
+    def longestMountain(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        n = len(A)
+        
+        if n <= 2:
+            return 0
+        
+        longest = 0
+        flag1 = 0                            # set flag when going up 
+        flag2 = 0                            # set flag when going down after going up 
+        
+        for i in range(n - 1):
+            d = A[i] - A[i+1]
+            
+            if flag2 == 1 and d <= 0:        # down->
+                right = i
+                tmp = right - left + 1       # calculate the length of mountain
+                if tmp > longest:
+                    longest = tmp
+                flag2 = 0
+            if flag1 == 0: 
+                if d < 0:                    # ->up
+                    left = i
+                    flag1 = 1
+            else:
+                if d > 0:                    # up->peak->down
+                    flag1 = 0
+                    flag2 = 1
+                if d == 0:                   # up->peak->flat
+                    flag1 = 0
+        
+        if flag2 == 1:
+            right = n-1
+            tmp = right - left + 1       # calculate the length of mountain
+            if tmp > longest:
+                longest = tmp
+        
+        return longest
