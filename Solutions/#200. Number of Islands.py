695fe8715137deb5bd475d9ddd3738109abad0cb
Wate <31439027+Watesoyan@users.noreply.github.com>
2018-07-02 22:01:57 +0800

    Update #200. Number of Islands.py

--- Solutions/#200. Number of Islands.py
+++ Solutions/#200. Number of Islands.py
@@ -5,55 +5,40 @@ class Solution(object):
         :rtype: int
         """
         m = len(grid)
+        num_Islands = 0
         
-        if not m :
-            return 0
-            
-        n = len(grid[0])
-        island_num = m * n
-        
-        dirs = ((1,0), (0,1))
-        pre = {(i,j):(i,j) for i in range(m) for j in range(n)}
-        
-        isvalid = lambda i,j: 0 <= i < m and 0 <= j < n
-        
-        for i in range(m):
-            for j in range(n):
+        for i0 in range(m):
+            n = len(grid[i0])
+            i = i0
+            for j0 in range(n):
+                j = j0
+                if grid[i][j] == '0':
+                    continue
                 
-                ij = (i, j)
+                grid[i][j] = '0'
+                stack = []
                 
-                if grid[i][j] == "0":
-                        island_num -= 1
-                    
-                else:
-                        
-                    for dir in dirs:
-                        ij_tmp = i_tmp, j_tmp = i+dir[0], j+dir[1]
-
-                        if isvalid(i_tmp, j_tmp) and grid[i_tmp][j_tmp] == "1":
-
-                            # find root of (i,j), using path splitting 
-                            ij_rank = 0
-                            ij_pre = ij
-                            while pre[ij_pre] is not ij_pre:
-                                ij_pre = pre[pre[ij_pre]]
-                                ij_rank += 1
-                            ij_root = ij_pre
-
-                            # find root of (i_tmp,j_tmp), using path splitting 
-                            ij_tmp_rank = 0
-                            ij_tmp_pre = ij_tmp
-                            while pre[ij_tmp_pre] is not ij_tmp_pre:
-                                ij_tmp_pre = pre[pre[ij_tmp_pre]]
-                                ij_tmp_rank += 1
-                            ij_tmp_root = ij_tmp_pre
-                            
-                            # merge 
-                            if ij_root != ij_tmp_root:
-                                island_num -= 1
-                                if ij_rank < ij_tmp_rank:
-                                    pre[ij_root] = ij_tmp_root
-                                else:
-                                    pre[ij_tmp_root] = ij_root
-        
-        return island_num
+                while True:
+                    if i != 0 and grid[i-1][j] == '1':
+                        stack.append((i,j))
+                        i -= 1
+                        grid[i][j] = '0'
+                    elif i != m-1 and grid[i+1][j] == '1':
+                        stack.append((i,j))
+                        i += 1
+                        grid[i][j] = '0'
+                    elif j != 0 and grid[i][j-1] == '1':
+                        stack.append((i,j))
+                        j -= 1
+                        grid[i][j] = '0'
+                    elif j != n-1 and grid[i][j+1] == '1':
+                        stack.append((i,j))
+                        j += 1
+                        grid[i][j] = '0'
+                    elif len(stack) > 0:
+                        (i, j) = stack.pop()
+                    else:
+                        break
+                num_Islands += 1
+                
+        return num_Islands
