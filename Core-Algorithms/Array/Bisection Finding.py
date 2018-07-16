905a3f7295024b8a9fd5f44198edc07016d36f8d
Wate <31439027+Watesoyan@users.noreply.github.com>
2018-07-16 09:14:09 +0800

    Update Bisection Finding.py

--- Core-Algorithms/Array/Bisection Finding.py
+++ Core-Algorithms/Array/Bisection Finding.py
@@ -1,17 +1,16 @@
 """
-bisect_find(array, x)
+bi_search(array, x)
 :input params:
 	array: ascending-order sorted, `List`
 	x: `int` or `float`
 :output params:
 	ix: if `x` is found in array, return the first index `ix` satisfying `array[ix] == x`, else return -1
 """
-def bisect_find(array, x):
-	n = len(array)
-	nmin, nmax = 0, n-1
+def bi_search(array, x):
+	nmin, nmax = 0, len(array)-1
 
 	while nmin != nmax:
-		mid = (nmin + nmax) // 2
+		mid = (nmin + nmax) >> 1
 		if array[mid] < x:
 			nmin = mid + 1
 		else:
