71ebfdf8e37d762326288be0e98f140c8cfb7738
Wate <31439027+Watesoyan@users.noreply.github.com>
2018-08-07 17:01:53 +0800

    Update #240. Search a 2D Matrix II.py

--- Solutions/Array/#240. Search a 2D Matrix II.py
+++ Solutions/Array/#240. Search a 2D Matrix II.py
@@ -5,19 +5,15 @@ class Solution:
         :type target: int
         :rtype: bool
         """
-        if not matrix:
-            return False
+        n = len(matrix)
         
-        m = len(matrix)
-        n = len(matrix[0])
-        
-        i, j = 0, n-1
-        
-        while i < m and j > -1:
-            if matrix[i][j] == target:
-                return True
-            elif matrix[i][j] < target:
-                i += 1
-            else:
-                j -= 1
+        if n > 0:
+            i, j = 0, len(matrix[0])-1
+            while j > -1 and i < len(matrix):
+                if matrix[i][j] == target:
+                    return True
+                elif matrix[i][j] < target:
+                    i += 1
+                else:
+                    j -= 1
         return False
