34b580710b28e57e1d0b2c49ec2c02eaf01dcbdd
Wate <31439027+Watesoyan@users.noreply.github.com>
2018-08-10 21:22:13 +0800

    Update Binary Tree Level Traversal.py

--- Core-Algorithms/Binary Tree/Binary Tree Level Traversal.py
+++ Core-Algorithms/Binary Tree/Binary Tree Level Traversal.py
@@ -3,32 +3,27 @@ Level Traversal
 """
 
 def LevelTraversal(self, root):
-	"""
-	:type root: TreeNode
-	:rtype: List[List[int]]
-	"""
-	if root is None:
-		return []
-	
-	nextroots = [root]  
-	result = [[root.val]]
-	
-	while True:
-		tmproots = nextroots
-		nextroots = []
-		tmpvals = []
-		
-		for tmproot in tmproots:
-		
-			if tmproot.left is not None:
-				tmpvals.append(tmproot.left.val)
-				nextroots.append(tmproot.left)
-		
-			if tmproot.right is not None:
-				tmpvals.append(tmproot.right.val)
-				nextroots.append(tmproot.right)
-		
-		if len(tmpvals) > 0:
-			result.append(tmpvals)
-		else:
-			return result
+    """
+    :type root: TreeNode
+    :rtype: List[List[int]]
+    """
+    res = []
+    if root is None:
+        return res
+    this_stack = [root]
+    next_stack = []
+    while len(this_stack) > 0:
+        n = len(this_stack)
+        level_res = [0] * n
+        for i in range(n):
+            tmp_root = this_stack[i]
+            level_res[i] = tmp_root.val
+            if tmp_root.left is not None:
+                next_stack.append(tmp_root.left)
+            if tmp_root.right is not None:
+                next_stack.append(tmp_root.right)
+
+        res.append(level_res)
+        this_stack = next_stack
+        next_stack = []
+    return res
