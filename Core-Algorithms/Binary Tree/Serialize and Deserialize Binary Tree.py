02eb4c7cef84d7478ac37f2011be1566b9ac9bba
Wate <31439027+Watesoyan@users.noreply.github.com>
2018-08-12 17:04:28 +0800

    Update Serialize and Deserialize Binary Tree.py

--- Core-Algorithms/Binary Tree/Serialize and Deserialize Binary Tree.py
+++ Core-Algorithms/Binary Tree/Serialize and Deserialize Binary Tree.py
@@ -1,67 +1,31 @@
-class Codec:
-
-    def serialize(self, root):
-        """Encodes a tree to a single string.
-        
-        :type root: TreeNode
-        :rtype: str
-        """
-        if root is None:
-            return []
-        
-        nextroots = [root]  
-        result = [root.val]
-        
-        while True:
-            tmproots = nextroots
-            nextroots = []
-            tmpvals = []
-            
-            for tmproot in tmproots:
-            
-                if tmproot.left is None:
-                    tmpvals.append(None)
+class Solution:
+    def Serialize(self, root):
+        this_stack = [root]
+        res = []
+        while len(this_stack) > 0:
+            next_stack = []
+            for tmp_root in this_stack:
+                if tmp_root is None:
+                    res.append(None)
                 else:
-                    tmpvals.append(tmproot.left.val)
-                    nextroots.append(tmproot.left)
-            
-                if tmproot.right is None:
-                    tmpvals.append(None)
-                else:
-                    tmpvals.append(tmproot.right.val)
-                    nextroots.append(tmproot.right)
-            
-            if len(tmpvals) is 0:
-                return result
-            else:
-                result += tmpvals
-
-    def deserialize(self, data):
-        """Decodes your encoded data to tree.
+                    res.append(tmp_root.val)
+                    next_stack.append(tmp_root.left)
+                    next_stack.append(tmp_root.right)   
+            this_stack = next_stack
+        return res
         
-        :type data: str
-        :rtype: TreeNode
-        """
-        if len(data) is 0:
+    def Deserialize(self, s):
+        n = len(s)
+        if n == 0:
             return None
-        
-        n = len(data)
-        
-        nodes = [None if x is None else TreeNode(x) for x in data]
-                
-        parentIdx = 0
-        childIdx = 1
-        
-        while childIdx < n:
-            parentnode = nodes[parentIdx]
-            if parentnode is None:
-                parentIdx += 1
-            else:
-                parentnode.left = nodes[childIdx]
-                if childIdx + 1 == n:
-                    return nodes[0]
-                parentnode.right = nodes[childIdx + 1]
-                parentIdx += 1
-                childIdx += 2
-        
+        nodes = [None if x is None else TreeNode(x) for x in s]
+        i, j = 0, 1
+        while i < j < n:
+            p = nodes[i]
+            if p is not None:
+                p.left = nodes[j]
+                j += 1
+                p.right = nodes[j]
+                j += 1
+            i += 1
         return nodes[0]
