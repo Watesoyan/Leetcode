00a444e6f1b5b538ce1d7aac01e2ac3f4bce7d32
Wate <31439027+Watesoyan@users.noreply.github.com>
2018-08-26 16:45:51 +0800

    Update #889. Construct Binary Tree from Preorder and Postorder Traversal.py

--- Solutions/Binary Tree/#889. Construct Binary Tree from Preorder and Postorder Traversal.py
+++ Solutions/Binary Tree/#889. Construct Binary Tree from Preorder and Postorder Traversal.py
@@ -1,3 +1,7 @@
+"""
+https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/description/
+"""
+
 class Solution:
     def constructFromPrePost(self, pre, post):
         """
