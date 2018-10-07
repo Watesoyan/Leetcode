dbe3abd80c788b4aedbb1bede854ffe33c1b8ec1
Wate <31439027+Watesoyan@users.noreply.github.com>
2018-10-07 07:24:21 +0000

    Update Bisearch.py

--- Core-Algorithms/Array/Bisearch.py
+++ Core-Algorithms/Array/Bisearch.py
@@ -1,22 +1,17 @@
 """
 bi_search(array, x)
 :input params:
-	array: ascending-order sorted, `List`
-	x: `int` or `float`
+    array: ascending-order sorted, `List`
+    x: `int` or `float`
 :output params:
-	ix: if `x` is found in array, return the first index `ix` satisfying `array[ix] == x`, else return -1
+    ix: if `x` is found in array, return the first index `ix` satisfying `array[ix] == x`, else return -1
 """
-def bi_search(array, x):
-	nmin, nmax = 0, len(array)-1
-
-	while nmin != nmax:
-		mid = (nmin + nmax) >> 1
-		if array[mid] < x:
-			nmin = mid + 1
-		else:
-			nmax = mid
-		
-	if array[nmin] == x:
-		return nmin
-	else:
-		return -1
+def bisearch(arr, x):
+    l, r = 0, len(arr) - 1
+    while l < r:
+        mid = (l + r) >> 1
+        if arr[mid] < x:
+            l = mid + 1
+        else:
+            r = mid
+    return l if arr[l] == x else -1
